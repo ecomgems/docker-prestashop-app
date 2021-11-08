0d95bb85381dc2cfc22e56115cea2fcc9ee94f4a
Igor Goltsov <riversy@gmail.com>
2021-11-08 13:37:07 +0300

    Add image ecomgems/prestashop-app:7.2

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
-(cd 7.1;docker build --no-cache -t ecomgems/prestashop-app:7.1 .)
-docker push ecomgems/prestashop-app:7.1
+#(cd 7.1;docker build --no-cache -t ecomgems/prestashop-app:7.1 .)
+#docker push ecomgems/prestashop-app:7.1
+(cd 7.2;docker build --no-cache -t ecomgems/prestashop-app:7.2 .)
+docker push ecomgems/prestashop-app:7.2
