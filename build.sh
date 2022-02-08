b37fd5c71782803cb050d8589f2c4227a6a2f755
Igor Goltsov <riversy@gmail.com>
2022-02-08 10:31:03 +0300

    Add 7.3 and 7.4 tags

--- build.sh
+++ build.sh
@@ -4,3 +4,7 @@
 docker push ecomgems/prestashop-app:7.1
 (cd 7.2;docker build --no-cache -t ecomgems/prestashop-app:7.2 .)
 docker push ecomgems/prestashop-app:7.2
+(cd 7.3;docker build --no-cache -t ecomgems/prestashop-app:7.3 .)
+docker push ecomgems/prestashop-app:7.3
+(cd 7.4;docker build --no-cache -t ecomgems/prestashop-app:7.4 .)
+docker push ecomgems/prestashop-app:7.4
