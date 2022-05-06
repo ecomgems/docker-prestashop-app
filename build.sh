6def2dad168f014ee7810e2a30320d9f784ea49f
Igor Goltsov <riversy@gmail.com>
2022-05-06 15:01:13 +0300

    Use XDebug 3 in the PHP 7 images, Add PHP 8.0 and 8.1 images

--- build.sh
+++ build.sh
@@ -8,3 +8,7 @@ docker push ecomgems/prestashop-app:7.2
 docker push ecomgems/prestashop-app:7.3
 (cd 7.4;docker build --no-cache -t ecomgems/prestashop-app:7.4 .)
 docker push ecomgems/prestashop-app:7.4
+(cd 8.0;docker build --no-cache -t ecomgems/prestashop-app:8.0 .)
+docker push ecomgems/prestashop-app:8.0
+(cd 8.1;docker build --no-cache -t ecomgems/prestashop-app:8.1 .)
+docker push ecomgems/prestashop-app:8.1
